11cf96965c1f4330f23aef4cd645497199cb3e0b
unknown <belalamr330@gmail.com>
2025-04-15 00:02:52 +0200

    Added my profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,19 @@
 import '../styles/Hero.css';
+import profilePic from '../assets/belal.jpg';
 
 function Hero() {
   return (
     <section id="hero" className="hero">
-  <div className="hero-content" data-aos="zoom-in">
-    <h1 className="hero-title">Hi, I'm Belal Amr 👋</h1>
-    <p className="hero-subtitle">Fullstack Developer | React & Laravel Specialist</p>
-    <div className="hero-buttons" data-aos="fade-up" data-aos-delay="300">
-      <a href="#projects" className="btn">View Projects</a>
-      <a href="#contact" className="btn btn-outline">Contact Me</a>
-    </div>
-  </div>
-</section>
-
+      <div className="hero-content" data-aos="zoom-in">
+        <img src={profilePic} alt="Belal Amr" className="hero-img" />
+        <h1 className="hero-title">Hi, I'm Belal Amr 👋</h1>
+        <p className="hero-subtitle">Fullstack Developer | React & Laravel Specialist</p>
+        <div className="hero-buttons" data-aos="fade-up" data-aos-delay="300">
+          <a href="#projects" className="btn">View Projects</a>
+          <a href="#contact" className="btn btn-outline">Contact Me</a>
+        </div>
+      </div>
+    </section>
   );
 }
 
